503a6ae1fb303ae6ca869d3009421a77f10673e7
AaronKanaron <65012247+AaronKanaron@users.noreply.github.com>
2021-09-05 23:30:39 +0200

    Delete spicetify directory

--- spicetify/script/getAuth.js
+++
@@ -1,5 +0,0 @@
-let auth = {
-	"Authorization": "Bearer BQDsrAuE4NPShDO3M9e-5LuxIf5uTbyTTp5LX1Js3g-kh2nl-hDFSPvgX6OB-BR0MAiG9Hcga5fZQpwBq1__jpES23HDwTQGPH7yQK5vXqlpEf48tI1Vdsq9PIdAScA3JivJSDJ6FPj3q64eGvtYXg6CP6UrcF-mRqQ"
-}
-
-let user_id = "3jxq19fsw69s4iudsxv6v5tjy"
